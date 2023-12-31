45bda042fea63443055db64408e2958e1a61ca1a
thisbavoreva <2001040+thisbavoreva@users.noreply.github.com>
2023-12-31 21:31:15 +0100

    First commit

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-use crate::renderer::{MupdfRenderer, RenderOptions, Renderer, PdfiumRenderer};
+use crate::renderer::{MupdfRenderer, RenderOptions, Renderer, PdfiumRenderer, XpdfRenderer};
 
 mod renderer;
 
 fn main() {
-    let file = std::fs::read("pdf_small.pdf").unwrap();
+    let file = std::fs::read("idefix.pdf").unwrap();
 
     let renderers: Vec<Box<dyn Renderer>> = vec![Box::from(MupdfRenderer::new()),
-                                                 Box::from(PdfiumRenderer::new())];
+                                                 Box::from(PdfiumRenderer::new()),
+    Box::from(XpdfRenderer::new())
+    ];
 
     for renderer in renderers {
         let result = renderer
-            .render(&file, &RenderOptions { scale: 3.5 })
+            .render(&file, &RenderOptions { scale: 1.0 })
             .unwrap();
 
         for (page, res) in result.iter().enumerate() {
--- src/renderer.rs
+++ src/renderer.rs
@@ -124,3 +124,63 @@ impl Renderer for MupdfRenderer {
         "mupdf".to_string()
     }
 }
+
+pub struct XpdfRenderer {}
+
+impl XpdfRenderer {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl Renderer for XpdfRenderer {
+    fn render(&self, buf: &[u8], options: &RenderOptions) -> Result<Vec<RenderedPage>, String> {
+        let dir = TempDir::new("xpdf").unwrap();
+        let input_path = dir.path().join("file.pdf");
+        let mut input_file = File::create(&input_path).unwrap();
+        input_file.write(buf).unwrap();
+
+        // Needed so that trailing slash is added
+        let mut dir_path = PathBuf::from(dir.path());
+        dir_path.push("");
+
+        println!("{:?}", input_path);
+        println!("{:?}", dir_path);
+
+        let out = Command::new("pdftopng")
+            .arg("-r")
+            .arg((72.0 * options.scale).to_string())
+            .arg(&input_path)
+            .arg(&dir_path)
+            .output()
+            .map_err(|_| "failed to run xpdf")?;
+
+        let mut out_files: Vec<(i32, PathBuf)> = fs::read_dir(dir.path())
+            .map_err(|_| "")?
+            .filter_map(|entry| entry.ok())
+            .map(|entry| entry.path())
+            .filter_map(|path| {
+                path.file_name()
+                    .and_then(|name| name.to_str())
+                    .and_then(|name| {
+                        let captures = regex::Regex::new(r"(?m)-(\d+).png")
+                            .unwrap()
+                            .captures(name)?;
+                        let num_str = captures.get(1)?;
+                        let num: i32 = num_str.as_str().parse().ok()?;
+                        Some((num, path.clone()))
+                    })
+            })
+            .collect::<Vec<_>>();
+
+        out_files.sort_by_key(|e| e.0);
+
+        let out_files = out_files.iter().map(|e| fs::read(&e.1).unwrap()).collect();
+
+        Ok(out_files)
+    }
+
+    fn name(&self) -> String {
+        "xpdf".to_string()
+    }
+}
