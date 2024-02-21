6442baabcbb4093b1a1e75ae70358b411202c735
thisbavoreva <2001040+thisbavoreva@users.noreply.github.com>
2024-02-21 17:33:45 +0100

    First decent version

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     let root_dir = Path::new("pdf");
 
     let files: Vec<_> = WalkDir::new(root_dir).into_iter().filter_map(|e| e.ok())
-        .filter(|e| e.file_type().is_file()).collect();
+        .filter(|e| e.file_type().is_file() && e.file_name().to_string_lossy().ends_with(".pdf")).collect();
 
     for entry in files {
         let pdf_path = entry.path();
@@ -39,8 +39,6 @@ fn main() {
             rendered_pages.push(result);
         }
 
-        let mut consolidated_images: Vec<u8> = vec![];
-
         for i in 0..rendered_pages[0].len() {
             let max_width = *[
                 imagesize::blob_size(&rendered_pages[0][i]).unwrap().width,
@@ -58,19 +56,21 @@ fn main() {
                 imagesize::blob_size(&rendered_pages[4][i]).unwrap().height,
             ].iter().max().unwrap();
 
-            let stroke_width = 3;
+            let stroke_width = 3.0;
 
             let path = {
                 let mut pb = PathBuilder::new();
                 pb.move_to(0.0, 0.0);
-                pb.line_to(max_width as f32, 0.0);
-                pb.line_to(max_width as f32, max_height as f32);
-                pb.line_to(0.0, max_height as f32);
+                pb.line_to(max_width as f32 + stroke_width, 0.0);
+                pb.line_to(max_width as f32 + stroke_width, max_height as f32 + stroke_width);
+                pb.line_to(0.0, max_height as f32 + stroke_width);
                 pb.close();
                 pb.finish().unwrap()
             };
 
-            let mut pixmap = Pixmap::new(((max_width + stroke_width) * 5) as u32, (max_height + stroke_width) as u32).unwrap();
+            let mut cursor = Transform::from_translate(stroke_width / 2.0, stroke_width / 2.0);
+
+            let mut pixmap = Pixmap::new(((max_width as f32 + stroke_width * 2.0) * 5.0) as u32, (max_height as f32 + stroke_width * 2.0) as u32).unwrap();
 
             let mut stroke = Stroke::default();
             stroke.width = stroke_width as f32;
@@ -79,21 +79,22 @@ fn main() {
                 let mut paint = Paint::default();
                 paint.set_color_rgba8(renderers[j].color().0, renderers[j].color().1, renderers[j].color().2, 255);
 
-                pixmap.draw_pixmap(((max_width * j) + stroke_width) as i32, 0, Pixmap::decode_png(&rendered_pages[j][i]).unwrap().as_ref(),
-                                   &PixmapPaint::default(), Transform::default(), None);
-                pixmap.stroke_path(&path, &paint, &stroke, Transform::from_translate((max_width * j + stroke_width) as f32, 0.0), None);
+                pixmap.draw_pixmap(0, 0, Pixmap::decode_png(&rendered_pages[j][i]).unwrap().as_ref(),
+                                   &PixmapPaint::default(), Transform::from_translate(cursor.tx + stroke_width / 2.0, cursor.ty + stroke_width /  2.0), None);
+
+                pixmap.stroke_path(&path, &paint, &stroke, cursor, None);
+
+                cursor.tx += max_width as f32 + stroke_width * 2.0;
             }
 
-            pixmap.save_png("out.png");
+            let mut dir = PathBuf::from("test");
+            dir.push(pdf_path.with_extension("").strip_prefix(root_dir).unwrap());
+            let mut path = dir.clone();
+            path.push(format!("{}.png", i));
+            let _ = std::fs::create_dir_all(dir);
+            pixmap.save_png(&path).unwrap();
         }
 
-        // for (page, res) in result.iter().enumerate() {
-        //     let mut dir = PathBuf::from("test");
-        //     dir.push(pdf_path.with_extension("").strip_prefix(root_dir).unwrap());
-        //     let mut path = dir.clone();
-        //     path.push(format!("{}-{}.png", page, renderer.name()));
-        //     let _ = std::fs::create_dir_all(dir);
-        //     std::fs::write(path, res).unwrap();
-        // }
+
     }
 }
