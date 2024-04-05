50e6a2380c5450b9f05a28c037d1478296227445
thisbavoreva <2001040+thisbavoreva@users.noreply.github.com>
2024-04-05 08:27:49 +0200

    change pdfium lib

--- src/pdfium/src/main.rs
+++ src/pdfium/src/main.rs
@@ -17,7 +17,9 @@ fn main() -> Result<(), String> {
     let file = std::fs::read(input_path).map_err(|_| "couldnt read input file")?;
 
     let pdfium =
-        Pdfium::new(Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name()).unwrap());
+        Pdfium::new( Pdfium::bind_to_library(Pdfium::pdfium_platform_library_name_at_path(
+            "/Users/lstampfl/lib/",
+        )).unwrap());
 
     let document = pdfium
         .load_pdf_from_byte_slice(&file, None)
